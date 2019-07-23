e058dd80eaa12b590b1b08eb94ceb23ba1e106a3
Rashad Attaf <rashadattaf@gmail.com>
2019-07-23 19:36:22 +0300

    fixing smooth scrolling and add opacity to the nav while scrolling

--- js/app.js
+++ js/app.js
@@ -20,3 +20,32 @@ initMap = () => {
     }
   ).addTo(newMap);
 };
+
+window.addEventListener("scroll", function() {
+  if (this.window.scrollY > 150) {
+    document.querySelector(".nav").style.opacity = 0.9;
+  } else {
+    document.querySelector(".nav").style.opacity = 1;
+  }
+});
+
+// Add smooth scrolling to all links
+$(".nav a, .header__container--btn").on("click", function(event) {
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    const hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 70
+      },
+      800
+    );
+  } // End if
+});
